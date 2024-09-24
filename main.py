2988eba5dec01a60671da8b758aae06484e2136d
regismika <regimantas.mikaliunas@gmail.com>
2024-09-24 21:40:54 +0300

    20240924

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-from lib2to3.pygram import python_grammar_no_print_statement
+# Liepkite vartotojui suvesti savo amžių. Patikrinkite ar amžius yra
+# didesnis arba lygus 18-ai, jei taip - išveskite "jūs galite balsuoti"
+#
+# print("Iveskite savo amziu")
+# skaicius = int(input())
+# if skaicius >= 18:
+#     print("Esate pilnametis ir galite balsuoti")
+# else: print("esate nepilnametis ir balsyoti negalite")
+#
+# Leiskite vartotojui suvesti norimą kiekį pažymių (pavyzdžiui, jūs
+# nusimatote 3-is kintamuosius, tai leidžiate padaryti 3 įvedimus). Raskite
+# šių pažymių vidurkį. Patikrinkite ar vidurkis teigiamas (daugiau arba lygu
+# 5-iems), jei taip - išveskite "vidurkis teigiamas"
+from xml.dom.minidom import ProcessingInstruction
 
-print("hello_world")
+# print("iveskite 1-a skaiciu")
+# skaicius1 = int(input())
+# print("iveskite 2-a skaiciu")
+# skaicius2 = int(input())
+# print("iveskite 3-a skaiciu")
+# skaicius3 = int(input())
+# print(f'jus ivedete {skaicius1}, {skaicius2}, {skaicius3}')
+# Pazymiu_Vidurkis = (skaicius1 + skaicius2 + skaicius3) / 3
+# print(f'jusu Pazymiu_vudirkis yra {Pazymiu_Vidurkis}')
+# if Pazymiu_Vidurkis >= 5:
+#     print("Pazimys teigiamas")
+# else:
+#     print("Pazimys neigiamas")
 
-if 7:
-    print("tells 'Laba diena'")
-else:
-    print("go home")
+# Susikurkite skaičiaus kintamąjį arba leiskite šį skaičių įvesti vartotojui.
+# Atlikite šiuos patikrinimus ir veiksmus:
+# 1. Jei skaičius dalinasi iš 5, tuomet išveskite šio skaičiaus daugybos lentelę nuo 1 iki 5.
+# 2. Jei skaičius lyginis, tuomet išveskite šį skaičių, jo kvadratą ir jį padalintą iš 2.
+# 3. Jei skaičius nesidalina iš 7, tuomet susikurkite antrąjį kintamąjį, išveskite šių dviejų skaičių
+# sumą, skirtumą, sandaugą, dalmenį
+
+# print("Iveskite skaicius")
+# skaicius = int(input())
+# if skaicius % 5 == 0:
+#     print("1 * 5 \n 2 * 5 \n 3 * 5 \n 4 * 5 \n 5 * 5")
+# elif skaicius % 2 == 0:
+#     result = (skaicius) * (skaicius) / 2
+#     print(result)
+# else:
+#    skaicius % 7 != 0
+#    print("Iveskite dar viena skaiciu")
+#    skaiciusX = int(input())
+#    suma = (skaicius) + (skaiciusX)
+#    skirtumas = (skaicius) - (skaiciusX)
+#    sandauga = (skaicius) * (skaiciusX)
+#    dalmuo = (skaicius) / (skaiciusX)
+#    print("suma:", (suma), "skirtumas:", (skirtumas), "sandauga:", (sandauga), "dalmuo:", (dalmuo))
+
